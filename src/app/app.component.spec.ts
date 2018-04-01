50fd9523714cb7019202fae64add8ce50354d361
Jonathan Baggaley <jbaggaley@gmail.com>
2018-04-01 23:23:45 +0100

    Code coverage now 100%

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,43 @@
-import { TestBed, async } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { TestBed, fakeAsync, tick, async } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationEnd,
+  NavigationError,
+  NavigationCancel
+} from '@angular/router';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './core/home/home.component';
 
 describe('AppComponent', () => {
+  let location: Location;
+  let router: Router;
+  let fixture;
+  const routes = AppRoutingModule.applicationRoutes;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        declarations: [
-          AppComponent,
-          HomeComponent
-        ],
-        imports: [
-           RouterTestingModule.withRoutes([
-            { path: '', component: HomeComponent },
-          ])
-        ]
-      });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes(routes)],
+      declarations: [HomeComponent, AppComponent]
+    });
 
-    })
-  );
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+
+    fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
+  });
+
   it(
     'should create the app',
     async(() => {
-      const fixture = TestBed.createComponent(AppComponent);
       const app = fixture.debugElement.componentInstance;
       expect(app).toBeTruthy();
     })
@@ -35,7 +45,6 @@ describe('AppComponent', () => {
   it(
     `should have as title 'Angular 5 material seed project'`,
     async(() => {
-      const fixture = TestBed.createComponent(AppComponent);
       const app = fixture.debugElement.componentInstance;
       expect(app.title).toEqual('Angular 5 material seed project');
     })
@@ -43,7 +52,6 @@ describe('AppComponent', () => {
   it(
     'should render title in a h1 tag',
     async(() => {
-      const fixture = TestBed.createComponent(AppComponent);
       fixture.detectChanges();
       const compiled = fixture.debugElement.nativeElement;
       expect(compiled.querySelector('h1').textContent).toContain(
